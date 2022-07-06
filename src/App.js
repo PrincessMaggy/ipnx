45d7645ec5bdd3c884e2155009c9fcf37e12d57f
PrincessMaggy <edoziemagdalene@gmail.com>
2022-07-06 11:05:16 +0100

    more assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Home from './pages/Home';
+import './styles/app.scss';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
