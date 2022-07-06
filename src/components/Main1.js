425bd9a831a1867589d0df92c352e4e50bc89b5e
PrincessMaggy <edoziemagdalene@gmail.com>
2022-07-06 14:09:07 +0100

    section 2

---
+++ src/components/Main1.js
@@ -0,0 +1,55 @@
+import React from "react";
+import man from '../images/man.jpg';
+import headphone from '../images/headphone.png';
+import speed from '../images/speed.png';
+import at from '../images/at.png';
+import pay from '../images/pay.png';
+
+
+const Main1 = () =>{
+    
+    return(
+        <div className="Main1">
+           <div className="section1">
+             <div className="one">
+                <p>Your business deserves the <br/>best connectivity</p>
+            </div>
+            <div className="two">
+                <img src={man} alt="man" />
+            </div>
+        </div>
+
+        <div className="section2">
+            <h2>How can we help you today?</h2>
+            <div className="four">
+                <div>
+                    <img src={headphone} alt="headphone"/>
+                    <h4>24/7 Customer Support</h4>
+                    <p> Find the answers you're looking for online or <br/> call on <b>+234-01-628 1111</b></p>
+                </div>
+
+                <div>
+                    <img src={speed} alt="speed"/>
+                    <h4>Speed Test</h4>
+                    <p>Test your internet speed...</p>
+                </div>
+
+                <div>
+                    <img className="pay" src={pay} alt="pay"/>
+                    <h4>Payment Channels</h4>
+                    <p>Pay online orby cash or POS via our Retail <br/> Outlets...</p>
+                </div>
+
+                <div>
+                    <img src={at} alt="at"/>
+                    <h4>Contact Us</h4>
+                    <p>Complete our contact form and we'll be in <br/> touchwith you ASAP!</p>
+                </div>
+        </div>
+    </div>
+
+    </div>
+    )
+}
+
+export default Main1;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Main1 from "../components/Main1";
 
 const Home =()=>{
 
     return(
         <div className="Home">
             <Navbar/>
+            <Main1/>
         </div>
     )
 }
