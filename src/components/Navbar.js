65a72e6d77766d60ee3d7ae2eee657e155038400
PrincessMaggy <edoziemagdalene@gmail.com>
2022-07-06 11:55:12 +0100

    desktop view of navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,41 @@ const Navbar =()=>{
 
     return(
         <div className="Navbar">
+            <div className="logo">
+                <p>ip<span>NX</span></p>
+            </div>
+            <div className="links">
+                <ul>
+                    <li>
+                        <a href="home.html">FOS XTREME</a>
+                    </li>
+                    <li>
+                        <a href="home.html">RESIDENTIAL</a>
+                    </li>
+                    <li>
+                        <a href="home.html">BUSINESS</a>
+                    </li>
+                    <li>
+                        <a href="home.html">SUPPORT</a>
+                    </li>
+                    <li>
+                        <a href="home.html">MY IPNX ACOUNT</a>
+                    </li>
+                    <li>
+                        <a href="home.html"> CARRERS </a>
+                    </li>
+                    <li>
+                        <a href="home.html"> ABOUT US</a>
+                    </li>
+                    <li>
+                        <a href="home.html"> CONTACT US</a>
+                    </li>
+                </ul>
+                
+            </div>
+            <div className="btn">
+                <button className="sign">Sign Up</button>
+            </div>
 
         </div>
     )
